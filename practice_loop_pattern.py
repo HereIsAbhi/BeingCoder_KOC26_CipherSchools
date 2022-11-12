5331e080db32fd5e89a6b7f08905d4e359313d36
HereIsAbhi <kabhinav201412@gmail.com>
2022-11-12 09:42:58 +0530

    pattern

---
+++ practice_loop_pattern.py
@@ -0,0 +1,35 @@
+# FIRST PATTERN:
+
+# i= 1
+# s= str( )
+# while i<=5 :
+#     s=  str(i)+" " +s
+#     print(s)
+#     i+=1
+
+# SECOND PATTERN:
+
+# i= 1
+# s= str( )
+# while i<=5 :
+#     s=  s+" " +str(i)
+#     print(s)
+#     i+=1   
+
+# i=5
+# s= str()
+# while i>=1:
+#     s= s+ " "+ str(i)
+#     print(s)
+#     i-=1
+
+# another pattern
+# i= 1
+# j=5 
+# s= str()
+# while i<=5:
+#     while j>=i:
+#         s= s + str(j)
+#         j-=1
+#     print(s)        
+#     i+=1  
